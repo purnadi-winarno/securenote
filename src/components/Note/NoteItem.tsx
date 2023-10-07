dd9d446a9395dcca4f3bd778bfd3281193b8a093
Purnadi <purnadi.winarno@automate-app.com>
2023-10-07 11:04:21 +0700

    add icon and animations

--- src/components/Note/NoteItem.tsx
+++ src/components/Note/NoteItem.tsx
@@ -1,7 +1,12 @@
 import React, {FC} from 'react';
 import {Text, StyleSheet} from 'react-native';
+import Animated, {Layout, FadeInLeft} from 'react-native-reanimated';
 import {TouchableOpacity} from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/AntDesign';
 import {NoteType} from 'src/types/noteType';
+
+const AnimatedTouchableOpacity =
+  Animated.createAnimatedComponent(TouchableOpacity);
 
 interface NoteItemProps {
   item: NoteType;
@@ -10,9 +15,14 @@ interface NoteItemProps {
 
 const NoteItem: FC<NoteItemProps> = ({item, index}) => {
   return (
-    <TouchableOpacity activeOpacity={0.9} style={styles.container}>
-      <Text>{item.value}</Text>
-    </TouchableOpacity>
+    <AnimatedTouchableOpacity
+      layout={Layout}
+      entering={FadeInLeft.delay(index * 100)}
+      activeOpacity={0.9}
+      style={styles.container}>
+      <Icon name="checkcircleo" size={16} />
+      <Text style={styles.text}>{item.value}</Text>
+    </AnimatedTouchableOpacity>
   );
 };
 const styles = StyleSheet.create({
@@ -24,6 +34,12 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     paddingVertical: 16,
     marginBottom: 8,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  text: {
+    flex: 1,
+    marginLeft: 10,
   },
 });
 export default NoteItem;
